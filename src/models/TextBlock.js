745f8f1fe6ee6160ac2ba4419cfa8dfb6d89dd38
jeisonj2019 <jeisonj2019@gmail.com>
2024-03-28 21:34:13 +0500

    Добавлен метод 'isinBoundary'

--- src/models/TextBlock.js
+++ src/models/TextBlock.js
@@ -1,9 +1,9 @@
 import { cnv } from "../shared/cnv.js";
-import {g as np} from "../shared/common.js";
-export class TextBlock{
+import { g as np } from "../shared/common.js";
+export class TextBlock {
 
     constructor(start, textArray, fontSize) {
-        this.start = {...start};
+        this.start = { ...start };
         this.textArray = [...textArray];
         this.fontSize = fontSize;
     }
@@ -21,8 +21,12 @@ export class TextBlock{
         var p2 = np(this.start.x + width, this.start.y - height);
         var p3 = np(this.start.x + width, this.start.y);
         var p4 = np(this.start.x, this.start.y);
-        return [p1, p2, p3, p4];
+        return { p1, p2, p3, p4 };
 
     }
 
+    isinBoundary(mouse) {
+        var { p1, p2, p3, p4 } = this.getBoundary();
+        return (p1.x <= mouse.x && p2.x >= mouse.x && p4.y >=mouse.y && p1.y<=mouse.y);
+    }
 }
